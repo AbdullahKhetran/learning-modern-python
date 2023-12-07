b81a8f141e40d2b8ea39baa32b005bbc2b92e2f2
Abdullah Khetran <khetranabdullah@gmail.com>
2023-12-07 22:38:13 +0500

    positive and negative indexing

--- 04_class(list_its_methods)/class4.py
+++ 04_class(list_its_methods)/class4.py
@@ -1,6 +1,7 @@
-
+#                       positive indexing
 # ->                    0        1         2
-names : list[str] = ["Qasim","Sir Zia", "Sir Inam"]
+names: list[str] = ["Qasim", "Sir Zia", "Sir Inam"]
 # <-                    -3     -2          -1
+#                       negative indexing
 
 print(names[-2])
